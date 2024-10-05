f92386f30ad5ce816725e46a2f307e1ddfd918be
Jinho Park <79356772+jinhodev@users.noreply.github.com>
2024-10-05 07:09:37 +0000

    main ui 4/5

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+      <header>
+        <div>
+          <h1>Stud</h1>
+        </div>
+        <div className='toggle'>
+          <div className='toggle_back'>
+            <div className='toggle_btn'>
+              Me
+            </div>
+          </div>
+        </div>
+        <div className='profile'></div>
       </header>
+
+      <div className='container'>
+
+        <div className='arrow'>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 16.2846L19.1925 8.57116" stroke="black" stroke-linecap="round"/>
+            <path d="M10 16.2846L19.1925 23.9981" stroke="black" stroke-linecap="round"/>
+          </svg>
+          <p>athor</p>
+        </div>
+     
+
+        <div className='album'>
+          <div className='album1'></div>
+          <div className='album1'></div>
+          <div className='album1'></div>
+          <div className='album1'></div>
+          <div className='album1'></div>
+          <div className='album1'></div>
+          <div className='album1'></div>
+        </div>
+      </div>
+
     </div>
   );
 }
