6f71b4d355da0c4ea42747d1f69afb5ed7bba10b
Jinho Park <79356772+jinhodev@users.noreply.github.com>
2024-10-15 09:59:09 +0000

    feat: 앨범 마우스 오버 기능 구현

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { useHorizontalScroll } from "../api/useSideScroll";
 
 function Album() {
   let a = [0,1,2,3,4,5,6,7];
+  const scrollRef = useHorizontalScroll();
 
+  // 각 앨범에 대한 hover 상태를 배열로 관리
+  const [hoveringIndex, setHoveringIndex] = useState(null);
+
+  const handleMouseOver = (index) => {
+    setHoveringIndex(index);
+  }
+
+  const handleMouseOut = () => {
+    setHoveringIndex(null);
+  }
 
   return (
-    <div className='album-container'>
-
-
-        {a.map(function(i){
-            return(
-            <div className='album' >
-                <img src={`/Album/ex${a[i]}.jpg`} alt="" className='album-img' />
+    <div className='album-container' ref={scrollRef} style={{ overflow: "auto" }}>
+      {a.map((i) => (
+        <div key={i}>
+          <div className={`album ${i}`}>
+            <img
+              src={`/Album/ex${i}.jpg`}
+              alt=""
+              className='album-img'
+              onMouseOver={() => handleMouseOver(i)}
+              onMouseOut={handleMouseOut}
+            />
+            {hoveringIndex === i ? (
+            <div className='mouseover_album'>
+              {/* 여기에 마우스 오버 시 보여줄 내용을 추가 */}
+              <p>Album {i} details</p>
             </div>
-            )
-        }
-        )}
+          ) : null}
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Album
+export default Album;
